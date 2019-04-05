ce8ec5fec5bddbdb0c8fac1ea6956c5169a65ffb
Joni Yrjänä <joniyrjana@gmail.com>
2019-04-05 12:31:18 +0300

    Fix LevelViewTest to be on the proper package.

--- FishingRodOfDestiny/src/test/java/fishingrodofdestiny/ui/widgets/LevelViewTest.java
+++ FishingRodOfDestiny/src/test/java/fishingrodofdestiny/ui/widgets/LevelViewTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package fishingrodofdestiny.ui;
+package fishingrodofdestiny.ui.widgets;
 
 import fishingrodofdestiny.ui.widgets.LevelView;
 import fishingrodofdestiny.world.Level;
