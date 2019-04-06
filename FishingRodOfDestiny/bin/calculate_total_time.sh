ac0771fcd45ea2c3f81c32686b77f9f0fe0241e7
Joni Yrjänä <joniyrjana@gmail.com>
2019-04-06 15:26:45 +0300

    Add bin/calculate_total_time.sh to calculate the total time from timesheet.

---
+++ FishingRodOfDestiny/bin/calculate_total_time.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MATH=$(for i in $(grep -E '^  <tr><td>' ../documentation/timesheet.md | cut --characters=25-28)
+       do
+           echo -n "${i}+"
+       done
+       echo "0"
+    )
+echo ${MATH}
+echo ${MATH} | bc -l
