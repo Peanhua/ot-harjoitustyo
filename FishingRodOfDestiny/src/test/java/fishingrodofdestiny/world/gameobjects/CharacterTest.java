be2fe2fb3113e0a1c7beae6c7823d6d061495663
Joni Yrjänä <joniyrjana@gmail.com>
2019-04-06 15:42:19 +0300

    Add simple test for the debug method Character.toString().

---
+++ FishingRodOfDestiny/src/test/java/fishingrodofdestiny/world/gameobjects/CharacterTest.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fishingrodofdestiny.world.gameobjects;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author joyr
+ */
+public class CharacterTest {
+    
+    private Character character;
+    
+    public CharacterTest() {
+    }
+    
+    @Before
+    public void setUp() {
+        this.character = new Character();
+    }
+    
+    @Test
+    public void toStringContainsAttack() {
+        assertTrue(this.character.toString().contains("attack"));
+    }
+}
