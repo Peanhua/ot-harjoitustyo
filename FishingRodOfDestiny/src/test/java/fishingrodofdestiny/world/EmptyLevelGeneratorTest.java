26186085dd8a8fed14441615a537351b04c12090
Joni Yrjänä <joniyrjana@gmail.com>
2019-04-06 11:36:13 +0300

    Add simple test for EmptyLevelGenerator.

---
+++ FishingRodOfDestiny/src/test/java/fishingrodofdestiny/world/EmptyLevelGeneratorTest.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fishingrodofdestiny.world;
+
+import java.util.Random;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author joyr
+ */
+public class EmptyLevelGeneratorTest {
+    
+    private EmptyLevelGenerator generator;
+    
+    public EmptyLevelGeneratorTest() {
+    }
+    
+    @Before
+    public void setUp() {
+        Random random = new Random(42);
+        this.generator = new EmptyLevelGenerator(random, 100, 100);
+    }
+    
+    @Test
+    public void canLevelBeCreated() {
+        Level level = this.generator.generateLevel(0);
+        assertTrue(level != null);
+    }
+}
